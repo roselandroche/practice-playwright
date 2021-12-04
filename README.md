c1c3b04a6591275ac236c4b450e4d30bfbff5f9e
Rose Landroche <r.landroche@gmail.com>
2021-12-04 12:57:47 -0800

    edit to README, start to build out tests for todos

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Practice using the Playwright testing library
 
-1. Create a basic [Playwright](https://playwright.dev/docs/library) test that goes to [netflix.com](https://www.netflix.com) and verifies that you cannot sign in with an invalid email/password combination. Add your code to the `index.js` file after the `// YOUR CODE STARTS` comment. The command `npm run test` will run your test.
+1. Create a basic [Playwright](https://playwright.dev/docs/library) test that goes to a URL and verifies functionality. Add your code to the `index.js` file after the `// YOUR CODE STARTS` comment. The command `npm run test` will run your test.
 
 2. Optional: If you want to add an assertion, you can use Playwright (example: [page.waitForSelector](https://playwright.dev/docs/api/class-page#page-wait-for-selector) or [page.waitForFunction](https://playwright.dev/docs/api/class-page#page-wait-for-function)).
--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const { chromium } = require("playwright");
   });
 
   // YOUR CODE STARTS
+  // test Netflix, cannot sign in with invalid credentials
+  // (async () => {
+  //   const page = await browser.newPage();
+  //   await page.goto("https://www.netflix.com");
+  //   await page.click('button:has-text("Close")');
+  //   await page.click('a:has-text("Sign In")');
+  //   await page.fill('input[name="userLoginId"]', "fakeUsername");
+  //   await page.fill('input[name="password"]', "abc123");
+  //   await page.click('button:has-text("Sign In")');
+  //   await page.waitForSelector("div:has-text('Please try again')");
+  // })();
+  // test React todos
   (async () => {
     const page = await browser.newPage();
-    await page.goto("https://www.netflix.com");
-    await page.click('button:has-text("Close")');
-    await page.click('a:has-text("Sign In")');
-    await page.fill('input[name="userLoginId"]', "fakeUsername");
-    await page.fill('input[name="password"]', "abc123");
-    await page.click('button:has-text("Sign In")');
-    await page.waitForSelector("div:has-text('Please try again')");
+    // navigate to page
+    await page.goto("https://todomvc.com/examples/react/#/");
   })();
   // YOUR CODE ENDS
 })();
